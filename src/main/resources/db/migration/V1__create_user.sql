614fc2fdc1bc5beaa3bbee951a51ddbcae75de24
Vitória Vitoria <vitoriasilva98@hotmail.com>
2026-03-13 23:14:49 -0300

    refactor: ajustando monitaramento e migration

--- src/main/resources/db/migration/V1__create_user.sql
+++ src/main/resources/db/migration/V1__create_user.sql
@@ -1,15 +1,15 @@
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id         UUID PRIMARY KEY,
     email      VARCHAR(255) UNIQUE NOT NULL,
     password   VARCHAR(255)        NOT NULL,
     name       VARCHAR(255)        NOT NULL,
-    birth_date DATE                NOT NULL,
-    phone      VARCHAR(20)         NOT NULL,
+    birth_date DATE,
+    phone      VARCHAR(20),
     role       INT                 NOT NULL,
     created_at TIMESTAMP           NOT NULL,
-    created_by BYTEA               NOT NULL,
+    created_by VARCHAR(255),
     updated_at TIMESTAMP,
-    updated_by BYTEA,
+    updated_by VARCHAR(255),
     deleted    BOOLEAN             NOT NULL DEFAULT FALSE
 );
